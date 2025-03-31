dc82affd117ddfd866e962acc19e7e0852a0a05d
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-03-31 13:52:13 -0500

    testing the app

--- storage.js
+++ storage.js
@@ -1,13 +1,16 @@
-class localStorageManager {
+class LocalStorageManager {
   saveRecentSearch(city) {
     let searches = this.getRecentSearches();
     if (!searches.includes(city)) {
       searches.unshift(city);
-      searches = searches.slice(0, 5);
+      searches = searches.slice(0, 5); // Keep last 5 searches
       localStorage.setItem("recentSearches", JSON.stringify(searches));
     }
   }
+
   getRecentSearches() {
     return JSON.parse(localStorage.getItem("recentSearches")) || [];
   }
 }
+
+export default new LocalStorageManager();
